19eab42f7934e845f4592b331e7847b6033e20b9
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-03-25 06:40:18 +0330

    Update DbBackupCommand.php

--- src/DbBackupCommand.php
+++ src/DbBackupCommand.php
@@ -229,3 +229,14 @@ class DbBackupCommand extends Command {
      * @return array
      */
 
+     protected function getOptions() {
+        return [
+            ['database', null, InputOption::VALUE_OPTIONAL, 'Database configuration name', null],
+            ['destination', null, InputOption::VALUE_OPTIONAL, 'Destination configuration name', null],
+            ['destinationPath', null, InputOption::VALUE_OPTIONAL, 'File destination path', null],
+            ['compression', null, InputOption::VALUE_OPTIONAL, 'Compression type', null],
+            ['timestamp', null, InputOption::VALUE_OPTIONAL, 'Append timestamp to filename',],
+        ];
+    }
+}
+
