4967f3a32d0c60047ca3be5e68011f385525e98a
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-03-26 07:46:19 +0330

    Update DbListCommand.php

--- src/DbListCommand.php
+++ src/DbListCommand.php
@@ -50,3 +50,35 @@ class DbListCommand extends Command {
         parent::__construct();
         $this->filesystems = $filesystems;
     }
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \LogicException
+     * @throws \BackupManager\Filesystems\FilesystemTypeNotSupported
+     * @throws \BackupManager\Config\ConfigFieldNotFound
+     * @throws \BackupManager\Config\ConfigNotFoundForConnection
+     * @return void
+     */
+    public function fire() {
+        if ($this->isMissingArguments()) {
+            $this->displayMissingArguments();
+            $this->promptForMissingArgumentValues();
+            $this->validateArguments();
+        }
+
+        $filesystem = $this->filesystems->get($this->option('source'));
+        $contents = $filesystem->listContents($this->option('path'));
+        $rows = [];
+        foreach ($contents as $file) {
+            if ($file['type'] == 'dir') continue;
+            $rows[] = [
+                $file['basename'],
+                key_exists('extension', $file) ? $file['extension'] : null,
+                $this->formatBytes($file['size']),
+                date('D j Y  H:i:s', $file['timestamp'])
+            ];
+        }
+        $this->table(['Name', 'Extension', 'Size', 'Created'], $rows);
+    }
+    
