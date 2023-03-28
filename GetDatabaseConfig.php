cac3d34cce9b7bd227324660dfc22890fe09cffa
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-03-28 06:12:18 +0330

    Create GetDatabaseConfig.php

---
+++ GetDatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php namespace BackupManager\Laravel;
+
+use BackupManager\Config\Config;
+
+/**
+ * Class GetDatabaseConfig
+ *
+ * @package BackupManager\Laravel
+ */
+trait GetDatabaseConfig
+{
+    /**
+     * @param $connections
+     * @return Config
+     */
